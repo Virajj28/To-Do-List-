0a831feb6ab5afe4ccfc29bf54aadf5887741d06
Virajj28 <viraj28j@gmail.com>
2022-02-20 18:47:42 +0530

    feat react-routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      Hello World !
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/list" element={<List />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home () {
+  return (
+    <>
+      <h1>Home</h1>
+      <Link to="/list">Go to list</Link>
+    </>
+  );
+}
+
+function List () {
+  return (
+    <>
+      <h1>List</h1>
+      <Link to="/">Go to home</Link>
+    </>
   );
 }
 
