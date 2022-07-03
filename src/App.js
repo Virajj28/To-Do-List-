4648d1a4e35db1745df1d3f2489fcc9feba9188a
Virajj28 <viraj28j@gmail.com>
2022-07-03 22:42:22 +0530

    main file w/ all routes navigated

--- src/App.js
+++ src/App.js
@@ -1,32 +1,21 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Todos from './components/Todos';
+import { AuthProvider, useAuth } from './auth';
+import { Login } from './components/Login';
+import { Home } from './components/Home';
 
 function App() {
+  const auth = useAuth();
   return (
-    <div className="App">
+    <AuthProvider>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/list" element={<List />} />
+        <div className="App">
+          <Route path = "/" element={<Home />} />
+          <Route path="/list" element={<Todos />} />
+        </div>
       </Routes>
-    </div>
-  );
-}
-
-function Home () {
-  return (
-    <>
-      <h1>Home</h1>
-      <Link to="/list">Go to list</Link>
-    </>
-  );
-}
-
-function List () {
-  return (
-    <>
-      <h1>List</h1>
-      <Link to="/">Go to home</Link>
-    </>
+    </AuthProvider>
   );
 }
 
